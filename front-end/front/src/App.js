e8e2042cadc18f763913d5fa8c2a9922f844078e
Aappo <93522510+Olvix@users.noreply.github.com>
2022-11-24 11:31:11 +0200

    Korjattu front-end näkymä

--- front-end/front/src/App.js
+++ front-end/front/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './App.css'
 import Data from './components/Data';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <Data></Data>
+    <div className="App">
+      <Data></Data>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+      
   );
 }
 
