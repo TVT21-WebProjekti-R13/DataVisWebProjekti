eecfcb0d4c25acbbf5b0fb9a69e9a71d7abe062d
Jan5u <janhenrik02@hotmail.fi>
2022-11-25 20:57:00 +0200

    Turhia kirjastoja ja koodeja pois

--- back-end/back/app.js
+++ back-end/back/app.js
@@ -1,65 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');  //KAtsotaan tarviiko
-var logger = require('morgan');               //KAtsotaan tarviiko
-var cors = require('cors')
-const testRouter = require("./routes/testRouter")   //TEST
-const mysql = require('mysql')    //Miksi tätä ei ollut aiemmin : D:D: 
+const express = require("express");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mysql = require("mysql2");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dataRouter = require('./routes/data');
-var app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const dataRouter = require("./routes/data");
+const testRouter = require("./routes/testRouter");
+const app = express();
 
-let PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors());
+app.use(helmet());
 
-app.use('/', indexRouter);
-app.use('/data', dataRouter);
-app.use('/users', usersRouter);
-app.use('/test', testRouter)      //TEST
+app.use("/", indexRouter);
+app.use("/data", dataRouter);
+app.use("/users", usersRouter);
+app.use("/test", testRouter);
 
 const db = mysql.createConnection({
   host: "127.0.0.1",
   port: "3306",
   user: "root",
-  password: "12345"
-})
-
-db.connect(function(err) {
-  if(err) throw err
-  console.log("db connected!")
-})
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+  password: "12345",
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("db connected!");
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on port 3001");
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
--- back-end/back/controllers/users.js
+++ back-end/back/controllers/users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
+  password: process.env.DB_PASS || "12345",
   database: process.env.DB_NAME || "sqltietokanta",
   waitForConnections: true,
   connectionLimit: 10,
