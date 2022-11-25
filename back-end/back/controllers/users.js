634b597e7653f1c358ad60f28f03164504c35383
Jan5u <janhenrik02@hotmail.fi>
2022-11-25 19:37:44 +0200

    käyttäjien luomisen ja tietokannan korjauksia

--- back-end/back/controllers/users.js
+++ back-end/back/controllers/users.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2/promise");
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
+
 const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
@@ -13,15 +13,10 @@ const db = mysql.createPool({
 
 const createUser = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, password, email, firstname, lastname } = req.body;
 
-
-    if (!username || !password) {
-      return res.status(400).send("Missing fields");
-    }
-
-    const [rows, fields] = await db.execute(
-      "SELECT * FROM käyttäjä WHERE username = ?",
+    const [rows, fields] = await db.query(
+      "SELECT * FROM käyttäjä WHERE Käyttäjänimi = ?",
       [username]
     );
 
@@ -29,15 +24,17 @@ const createUser = async (req, res) => {
       return res.status(400).json({ message: "Username already exists" });
     }
 
-    const saltRounds = 10;
-    const hash = bcrypt.hashSync(password, saltRounds);
-    await db.execute(
-      "INSERT INTO käyttäjä (username, email, password) VALUES (?, ?, ?)",
-      [username, email, password]
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    await db.query(
+      "INSERT INTO käyttäjä (Käyttäjänimi, sPosti, Etunimi, Sukunimi, SalasanaSalt) VALUES (?, ?, ?, ?, ?)",
+      [username, email, firstname, lastname, hashedPassword]
     );
+
     res.status(200).json({ message: "User created" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
 
